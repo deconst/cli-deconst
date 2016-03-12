6c88cba01507cd944a8403ccc50a51ec1019c8a3
Anne Gentle <annegentle@justwriteclick.com>
2016-03-12 08:05:39 -0600

    Can pass in a github url to get content info

--- deconst/cli.py
+++ deconst/cli.py
@@ -1,16 +1,38 @@
+# Copyright (c) 2016 Rackspace, Inc.
+#
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import click
+import json
+import requests
+from requests.utils import quote
+from tabulate import tabulate
+
 
 @click.group()
 @click.version_option()
-
 def cli():
     """Deconst, docs deconstructed.
     
     This is the command-line script for using the content API for
     developer.rackspace.com.
     """
+    output = "fred"
+    print(output)
 
-@cli.group()
 def deconst():
     """Command for calling the Content API read-only operations."""
 
@@ -61,4 +83,4 @@ def deconst_list():
     return [quote(item, safe='') for item in allContentIds]
 
 if __name__ == '__main__':
-    deconst()
+    cli()
